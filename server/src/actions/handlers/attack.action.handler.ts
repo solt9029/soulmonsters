230627cdad25e63cd3e90076b8d0b430ae91df3a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-19 02:31:31 +0900

    Refactoring

--- server/src/actions/handlers/attack.action.handler.ts
+++ server/src/actions/handlers/attack.action.handler.ts
@@ -8,6 +8,14 @@ import { GameUserRepository } from '../../repositories/game.user.repository';
 import { GameActionDispatchInput, BattlePosition, StateType } from '../../graphql/index';
 import { EntityManager } from 'typeorm';
 
+const calcNewSoulGameCardPosition = (gameEntity: GameEntity, userId: string): number => {
+  const soulGameCards = gameEntity.gameCards
+    .filter(value => value.zone === Zone.SOUL && value.currentUserId === userId)
+    .sort((a, b) => b.position - a.position);
+
+  return soulGameCards.length > 0 ? soulGameCards[0].position + 1 : 0;
+};
+
 export async function handleAttackAction(
   manager: EntityManager,
   userId: string,
@@ -29,20 +37,15 @@ export async function handleAttackAction(
   } else {
     const targetGameCard = gameEntity.gameCards.find(value => value.id === data.payload.targetGameCardIds[0]);
     const yourGameUser = gameEntity.gameUsers.find(value => value.userId === userId);
-    const yourSoulGameCards = gameEntity.gameCards
-      .filter(value => value.zone === Zone.SOUL && value.currentUserId === userId)
-      .sort((a, b) => b.position - a.position);
-    const yourSoulGameCardMaxPosition = yourSoulGameCards.length > 0 ? yourSoulGameCards[0].position : -1;
-    const opponentSoulGameCards = gameEntity.gameCards
-      .filter(value => value.zone === Zone.SOUL && value.currentUserId !== userId)
-      .sort((a, b) => b.position - a.position);
-    const opponentSoulGameCardMaxPosition = opponentSoulGameCards.length > 0 ? opponentSoulGameCards[0].position : -1;
+
+    const newYourSoulGameCardPosition = calcNewSoulGameCardPosition(gameEntity, userId);
+    const newOpponentSoulGameCardPosition = calcNewSoulGameCardPosition(gameEntity, opponentGameUser.userId);
 
     if (targetGameCard.battlePosition === BattlePosition.ATTACK) {
       if (gameCard.attack >= targetGameCard.attack) {
         await gameCardRepository.update(
           { id: targetGameCard.id },
-          { position: opponentSoulGameCardMaxPosition + 1, zone: Zone.SOUL },
+          { position: newOpponentSoulGameCardPosition, zone: Zone.SOUL },
         );
         await gameCardRepository.query(
           `UPDATE gameCards SET position = position - 1 WHERE gameId = ${gameEntity.id} AND zone = "BATTLE" AND currentUserId = "${opponentGameUser.id}" AND position > ${targetGameCard.position} ORDER BY position`,
@@ -54,7 +57,7 @@ export async function handleAttackAction(
       if (gameCard.attack <= targetGameCard.attack) {
         await gameCardRepository.update(
           { id: gameCard.id },
-          { position: yourSoulGameCardMaxPosition + 1, zone: Zone.SOUL },
+          { position: newYourSoulGameCardPosition, zone: Zone.SOUL },
         );
         await gameCardRepository.query(
           `UPDATE gameCards SET position = position - 1 WHERE gameId = ${gameEntity.id} AND zone = "BATTLE" AND currentUserId = "${yourGameUser.id}" AND position > ${gameCard.position} ORDER BY position`,
@@ -76,7 +79,7 @@ export async function handleAttackAction(
       if (gameCard.attack > targetGameCard.defence) {
         await gameCardRepository.update(
           { id: targetGameCard.id },
-          { position: opponentSoulGameCardMaxPosition + 1, zone: Zone.SOUL },
+          { position: newOpponentSoulGameCardPosition, zone: Zone.SOUL },
         );
         await gameCardRepository.query(
           `UPDATE gameCards SET position = position - 1 WHERE gameId = ${gameEntity.id} AND zone = "BATTLE" AND currentUserId = "${opponentGameUser.id}" AND position > ${targetGameCard.position} ORDER BY position`,
@@ -103,14 +106,14 @@ export async function handleAttackAction(
   const attackCountGameState = gameStates.find(gameState => gameState.state.type === StateType.ATTACK_COUNT);
   if (updatedGameCard.zone === Zone.BATTLE) {
     if (attackCountGameState === undefined) {
-      gameStateRepository.insert({
+      await gameStateRepository.insert({
         game: gameEntity,
         gameCard: updatedGameCard,
         state: { type: StateType.ATTACK_COUNT, data: { value: 1 } },
       });
       return;
     }
-    gameStateRepository.update(
+    await gameStateRepository.update(
       { id: attackCountGameState.id },
       {
         state: {
