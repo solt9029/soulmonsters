90289331c79d2d835ac2f3954163da28a37b431f
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-18 14:21:48 +0900

    Ignore

--- client-new/eslint.config.js
+++ client-new/eslint.config.js
@@ -7,7 +7,7 @@ import { globalIgnores } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/graphql/generated/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
