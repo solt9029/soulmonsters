3d71cad94f7fdc22ead4efae7cc912b45477092e
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-19 10:50:31 +0900

    Extract private method

--- server/src/actions/handlers/summon.monster.action.handler.ts
+++ server/src/actions/handlers/summon.monster.action.handler.ts
@@ -5,6 +5,14 @@ import { GameActionDispatchInput, BattlePosition } from '../../graphql/index';
 import { GameEntity } from '../../entities/game.entity';
 import { EntityManager } from 'typeorm';
 
+const calcNewBattleGameCardPosition = (gameEntity: GameEntity, userId: string): number => {
+  const battleGameCards = gameEntity.gameCards
+    .filter(value => value.zone === Zone.BATTLE && value.currentUserId === userId)
+    .sort((a, b) => b.position - a.position);
+
+  return battleGameCards.length > 0 ? battleGameCards[0].position + 1 : 0;
+};
+
 export async function handleSummonMonsterAction(
   manager: EntityManager,
   userId: string,
@@ -21,16 +29,13 @@ export async function handleSummonMonsterAction(
     `UPDATE gameUsers SET energy = energy - ${gameCard.card.cost} WHERE gameId = ${gameEntity.id} AND userId = '${userId}'`,
   );
 
-  const yourBattleGameCards = gameEntity.gameCards
-    .filter(value => value.zone === Zone.BATTLE && value.currentUserId === userId)
-    .sort((a, b) => b.position - a.position);
-  const yourBattleGameCardMaxPosition = yourBattleGameCards.length > 0 ? yourBattleGameCards[0].position : -1;
+  const newBattleGameCardPosition = calcNewBattleGameCardPosition(gameEntity, userId);
 
   // put the target monster card on your battle zone
   await gameCardRepository.update(
     { id: data.payload.gameCardId },
     {
-      position: yourBattleGameCardMaxPosition + 1,
+      position: newBattleGameCardPosition,
       zone: Zone.BATTLE,
       battlePosition: BattlePosition.ATTACK, // TODO: make this param selectable
     },
