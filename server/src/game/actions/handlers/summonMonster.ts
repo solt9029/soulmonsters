d9122610e5a220fae87c76cf44d012647b6d8947
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-20 23:25:49 +0900

    Refactor summon

--- server/src/game/actions/handlers/summonMonster.ts
+++ server/src/game/actions/handlers/summonMonster.ts
@@ -1,6 +1,5 @@
 import { Zone } from 'src/graphql';
 import { GameCardRepository } from '../../../repositories/game.card.repository';
-import { GameUserRepository } from '../../../repositories/game.user.repository';
 import { GameActionDispatchInput, BattlePosition } from '../../../graphql/index';
 import { GameEntity } from '../../../entities/game.entity';
 import { EntityManager } from 'typeorm';
@@ -13,35 +12,40 @@ const calcNewBattleGameCardPosition = (gameEntity: GameEntity, userId: string): 
   return battleGameCards.length > 0 ? battleGameCards[0].position + 1 : 0;
 };
 
+const subtractUserEnergy = (gameEntity: GameEntity, userId: string, amount: number): GameEntity => {
+  const gameUsers = gameEntity.gameUsers.map(gameUser =>
+    gameUser.userId === userId ? { ...gameUser, energy: gameUser.energy - amount } : { ...gameUser },
+  );
+  return { ...gameEntity, gameUsers };
+};
+
+const summonGameCard = (gameEntity: GameEntity, userId: string, gameCardId: number): GameEntity => {
+  const gameCards = gameEntity.gameCards.map(gameCard =>
+    gameCard.id === gameCardId
+      ? {
+          ...gameCard,
+          position: calcNewBattleGameCardPosition(gameEntity, userId),
+          zone: Zone.BATTLE,
+          battlePosition: BattlePosition.ATTACK,
+        }
+      : { ...gameCard },
+  );
+  return { ...gameEntity, gameCards };
+};
+
 export async function handleSummonMonsterAction(
   manager: EntityManager,
   userId: string,
   data: GameActionDispatchInput,
   gameEntity: GameEntity,
 ) {
+  const gameCard = gameEntity.gameCards.find(value => value.id === data.payload.gameCardId)!;
+  const originalPosition = gameCard.position;
+
+  gameEntity = subtractUserEnergy(gameEntity, userId, gameCard.card.cost);
+  gameEntity = summonGameCard(gameEntity, userId, data.payload.gameCardId!);
+  await manager.save(GameEntity, gameEntity);
+
   const gameCardRepository = manager.withRepository(GameCardRepository);
-  const gameUserRepository = manager.withRepository(GameUserRepository);
-
-  const gameCard = gameEntity.gameCards.find(value => value.id === data.payload.gameCardId);
-
-  if (!gameCard) {
-    throw new Error('Game card not found');
-  }
-
-  if (data.payload.gameCardId == null) {
-    throw new Error('Game card ID is null');
-  }
-
-  await gameUserRepository.subtractEnergy(gameEntity.id, userId, gameCard.card.cost);
-
-  await gameCardRepository.update(
-    { id: data.payload.gameCardId },
-    {
-      position: calcNewBattleGameCardPosition(gameEntity, userId),
-      zone: Zone.BATTLE,
-      battlePosition: BattlePosition.ATTACK, // TODO: make this param selectable
-    },
-  );
-
-  await gameCardRepository.packHandPositions(gameEntity.id, userId, gameCard.position);
+  await gameCardRepository.packHandPositions(gameEntity.id, userId, originalPosition);
 }
