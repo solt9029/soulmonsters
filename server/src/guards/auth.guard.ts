5d9b71ac68f4bc0eb39df9d8918f24482bfe6dec
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-21 09:47:40 +0900

    Fix

--- server/src/guards/auth.guard.ts
+++ server/src/guards/auth.guard.ts
@@ -15,7 +15,7 @@ const {
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: FIREBASE_PROJECT_ID,
-    privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    privateKey: FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     clientEmail: FIREBASE_CLIENT_EMAIL,
   }),
   databaseURL: FIREBASE_DATABASE_URL,
