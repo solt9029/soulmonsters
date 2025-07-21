d5c16e9964ded0f7b26c0599011c86b5ec1d3642
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-21 20:14:56 +0900

    Delete file

--- server/src/game/actions/handlers/attack/destroyMonster.ts
+++ server/src/game/actions/handlers/attack/destroyMonster.ts
@@ -1,6 +1,13 @@
 import { GameEntity } from 'src/entities/game.entity';
 import { Zone } from 'src/graphql';
-import { calcNewSoulGameCardPosition } from 'src/game/actions/handlers/attack/calcNewSoulGameCardPosition';
+
+const calcNewSoulGameCardPosition = (gameEntity: GameEntity, userId: string): number => {
+  const soulGameCards = gameEntity.gameCards
+    .filter(value => value.zone === Zone.SOUL && value.currentUserId === userId)
+    .sort((a, b) => b.position - a.position);
+
+  return soulGameCards.length > 0 ? soulGameCards[0].position + 1 : 0;
+};
 
 export const destroyMonster = (gameEntity: GameEntity, gameCardId: number): GameEntity => {
   const index = gameEntity.gameCards.findIndex(gameCard => gameCard.id === gameCardId);
