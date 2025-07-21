f5282f7570490da94d40bf62c9607ffc1e5b770b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-21 09:57:06 +0900

    Add helper script

--- server/scripts/console.ts
+++ server/scripts/console.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/modules/app.module';
 import { CardService } from '../src/services/card.service';
 import { DeckService } from '../src/services/deck.service';
+import { DeckCardService } from '../src/services/deck.card.service';
 import { GameService } from '../src/services/game.service';
 import { UserService } from '../src/services/user.service';
 import { GameRepository } from '../src/repositories/game.repository';
@@ -15,26 +16,44 @@ import * as repl from 'repl';
 async function bootstrap() {
   console.log('Starting Nest.js Console...');
 
-  const app = await NestFactory.createApplicationContext(AppModule);
-  const dataSource = app.get(DataSource);
-
   const replServer = repl.start({
     prompt: 'nest> ',
     useColors: true,
   });
 
-  // Add variables to REPL context
-  replServer.context.cardService = app.get(CardService);
-  replServer.context.deckService = app.get(DeckService);
-  replServer.context.gameService = app.get(GameService);
-  replServer.context.userService = app.get(UserService);
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const dataSource = app.get(DataSource);
+  const cardService = app.get(CardService);
+  const deckService = app.get(DeckService);
+  const deckCardService = app.get(DeckCardService);
+  const gameService = app.get(GameService);
+  const userService = app.get(UserService);
+
+  const createDebugDeck = async (userId: string) => {
+    const deckName = `${new Date().toISOString()}`;
+    const deck = await deckService.create(userId, deckName);
+
+    for (let cardId = 1; cardId <= 14; cardId++) {
+      const deckCard = await deckCardService.create(deck.id, cardId);
+      await deckCardService.updateCountById(deckCard.id, 3);
+    }
+
+    return deck;
+  };
+
+  replServer.context.app = app;
+  replServer.context.dataSource = dataSource;
+  replServer.context.cardService = cardService;
+  replServer.context.deckService = deckService;
+  replServer.context.deckCardService = deckCardService;
+  replServer.context.gameService = gameService;
+  replServer.context.userService = userService;
   replServer.context.gameRepository = GameRepository;
   replServer.context.gameUserRepository = GameUserRepository;
   replServer.context.gameCardRepository = GameCardRepository;
   replServer.context.gameStateRepository = GameStateRepository;
   replServer.context.deckCardRepository = DeckCardRepository;
-  replServer.context.dataSource = dataSource;
-  replServer.context.app = app;
+  replServer.context.createDebugDeck = createDebugDeck;
 
   // Handle REPL exit
   replServer.on('exit', async () => {
