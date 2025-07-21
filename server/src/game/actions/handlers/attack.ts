2dd208c8a3939e8f44517318fa3e26a4192d408d
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-21 20:08:07 +0900

    Refactor attack

--- server/src/game/actions/handlers/attack.ts
+++ server/src/game/actions/handlers/attack.ts
@@ -12,16 +12,7 @@ export async function handleAttackAction(
   data: GameActionDispatchInput,
   gameEntity: GameEntity,
 ) {
-  const gameCard = gameEntity.gameCards.find(value => value.id === data.payload.gameCardId);
-  const opponentGameUser = gameEntity.gameUsers.find(value => value.userId !== userId);
-
-  if (!gameCard) {
-    throw new Error('Game card not found');
-  }
-
-  if (!opponentGameUser) {
-    throw new Error('Opponent game user not found');
-  }
+  const opponentGameUser = gameEntity.gameUsers.find(value => value.userId !== userId)!;
 
   if (data.payload.gameCardId == null) {
     throw new Error('Game card ID is null');
@@ -30,26 +21,31 @@ export async function handleAttackAction(
   if (data.payload.targetGameUserIds?.length === 1) {
     directAttack(gameEntity, data.payload.gameCardId, opponentGameUser.userId);
     incrementAttackCount(gameEntity, data.payload.gameCardId);
-
-    console.log(gameEntity.gameStates);
-
     await manager.save(GameEntity, gameEntity);
     return;
   }
 
-  if (!data.payload.targetGameCardIds || data.payload.targetGameCardIds.length === 0) {
+  const targetGameCardId = data.payload.targetGameCardIds?.[0];
+
+  if (targetGameCardId == undefined) {
     throw new Error('Target game card IDs not provided');
   }
 
-  const targetGameCardId = data.payload.targetGameCardIds[0];
+  const originalGameCardPosition = gameEntity.gameCards.find(value => value.id === data.payload.gameCardId)?.position;
+  const originalTargetGameCardPosition = gameEntity.gameCards.find(card => card.id === targetGameCardId)?.position;
 
   monsterBattle(gameEntity, data.payload.gameCardId, targetGameCardId);
   incrementAttackCount(gameEntity, data.payload.gameCardId);
   await manager.save(GameEntity, gameEntity);
 
-  // TODO: packBattleZonePositionsの呼び出しをここに追加する
-  // もともとのgameCardのpositionを最初に保持しておく
-  // もともとのtargetGameCardのpositionを最初に保持しておく
-  // もし、gameCardのzoneがBATTLEではなかったなら、packBattleZonePositionsを呼び出す
-  // もし、targetGameCardのzoneがBATTLEではなかったなら、packBattleZonePositionsを呼び出す
+  const updatedGameCardZone = gameEntity.gameCards.find(card => card.id === data.payload.gameCardId)?.zone;
+  const updatedTargetGameCardZone = gameEntity.gameCards.find(card => card.id === targetGameCardId)?.zone;
+
+  if (updatedGameCardZone !== 'BATTLE' && originalGameCardPosition) {
+    await packBattleZonePositions(manager, gameEntity.id, userId, originalGameCardPosition);
+  }
+
+  if (updatedTargetGameCardZone !== 'BATTLE' && originalTargetGameCardPosition) {
+    await packBattleZonePositions(manager, gameEntity.id, opponentGameUser.userId, originalTargetGameCardPosition);
+  }
 }
