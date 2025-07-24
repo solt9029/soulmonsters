cfa48e5d04adb8e6de42a5c7508164f651c9f039
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-07-24 16:36:57 +0900

    Refactor

--- server/src/game/actions/validators/startDrawTime.ts
+++ server/src/game/actions/validators/startDrawTime.ts
@@ -3,8 +3,9 @@ import { GameEntity } from '../../../entities/game.entity';
 import { BadRequestException } from '@nestjs/common';
 
 export function validateStartDrawTimeAction(gameEntity: GameEntity, userId: string) {
-  const yourGameUser = gameEntity.gameUsers.find(value => value.userId === userId);
-  if (!yourGameUser?.actionTypes.includes(ActionType.START_DRAW_TIME)) {
+  const gameUser = gameEntity.gameUsers.find(value => value.userId === userId);
+
+  if (!gameUser?.actionTypes.includes(ActionType.START_DRAW_TIME)) {
     throw new BadRequestException();
   }
 }
--- server/src/game/actions/validators/startEnergyTime.ts
+++ server/src/game/actions/validators/startEnergyTime.ts
@@ -3,8 +3,9 @@ import { GameEntity } from '../../../entities/game.entity';
 import { BadRequestException } from '@nestjs/common';
 
 export function validateStartEnergyTimeAction(game: GameEntity, userId: string) {
-  const yourGameUser = game.gameUsers.find(value => value.userId === userId);
-  if (!yourGameUser?.actionTypes.includes(ActionType.START_ENERGY_TIME)) {
+  const gameUser = game.gameUsers.find(value => value.userId === userId);
+
+  if (!gameUser?.actionTypes.includes(ActionType.START_ENERGY_TIME)) {
     throw new BadRequestException();
   }
 }
