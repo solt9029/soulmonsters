c9faf967ceaa3aa9aa5d0fe4dce19bb62d6e8472
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-11-27 18:55:58 +0900

    Add presenters

---
+++ server/src/presenters/game.card.presenter.ts
@@ -0,0 +1,25 @@
+import { GameCard } from '../graphql';
+import { GameCardEntity } from '../entities/game.card.entity';
+
+export class GameCardPresenter {
+  static present(entity: GameCardEntity): GameCard {
+    return {
+      id: entity.id,
+      originalUserId: entity.originalUserId,
+      currentUserId: entity.currentUserId,
+      zone: entity.zone,
+      position: entity.position,
+      battlePosition: entity.battlePosition,
+      name: entity.name,
+      kind: entity.kind,
+      type: entity.type,
+      attribute: entity.attribute,
+      attack: entity.attack,
+      defence: entity.defence,
+      cost: entity.cost,
+      detail: entity.detail,
+      card: entity.card,
+      actionTypes: entity.actionTypes,
+    };
+  }
+}
