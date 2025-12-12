1941b734190bfc87ccf31165f1682de7029865f6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-12-12 15:55:20 +0900

    Revert

--- server/src/game/actions/handlers/startDrawTime.ts
+++ server/src/game/actions/handlers/startDrawTime.ts
@@ -4,8 +4,8 @@ import { drawCardFromDeck } from './startDrawTime/drawCardFromDeck';
 import { updateGamePhaseAndTurn } from './startDrawTime/updateGamePhaseAndTurn';
 
 export async function handleStartDrawTimeAction(manager: EntityManager, userId: string, gameModel: GameModel) {
-  updateGamePhaseAndTurn(gameModel);
-  drawCardFromDeck(gameModel, userId);
+  gameModel = updateGamePhaseAndTurn(gameModel);
+  gameModel = drawCardFromDeck(gameModel, userId);
 
   await manager.save(gameModel.toEntity());
 }
