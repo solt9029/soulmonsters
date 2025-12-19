6fcbe8d5fa887d90a898067d6c45efe43f9873c9
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-12-20 00:49:16 +0900

    Update

--- server/src/game/actions/handlers/index.ts
+++ server/src/game/actions/handlers/index.ts
@@ -5,7 +5,7 @@ import { ActionType } from '../../../graphql/index';
 import { handleStartDrawTimeAction } from './startDrawTime';
 import { handleStartEnergyTimeAction } from './startEnergyTime';
 import { handleStartPutTimeAction } from './startPutTime';
-import { handlePutSoulAction, PutSoulActionPayload } from './putSoul';
+import { handlePutSoulAction } from './putSoul';
 import { handleStartSomethingTimeAction } from './startSomethingTime';
 import { handleSummonMonsterAction } from './summonMonster';
 import { handleStartBattleTimeAction } from './startBattleTime';
@@ -13,63 +13,46 @@ import { handleStartEndTimeAction } from './startEndTime';
 import { handleAttackAction } from './attack';
 import { handleFinishEndTimeAction } from './finishEndTime';
 import { handleEffectRuteruteDraw } from './effectRuteruteDraw';
-import { validateStartDrawTimeAction } from '../validators/startDrawTime';
-import { validateStartEnergyTimeAction } from '../validators/startEnergyTime';
-import { validateStartPutTimeAction } from '../validators/startPutTime';
-import { validatePutSoulAction } from '../validators/putSoul';
-import { validateAttackAction } from '../validators/attack';
-import { validateStartEndTimeAction } from '../validators/startEndTime';
-import { validateStartSomethingTimeAction } from '../validators/startSomethingTime';
-import { validateStartBattleTimeAction } from '../validators/startBattleTime';
-import { validateFinishEndTimeAction } from '../validators/finishEndTime';
-import { validateEffectRuteruteDrawAction } from '../validators/effectRuteruteDraw';
+import { validateAction } from '../validators';
 
 @Injectable()
 export class GameActionHandler {
   handleAction(data: GameActionDispatchInput, userId: string, gameModel: GameModel) {
-    switch (data.type) {
+    const validationResult = validateAction(data, userId, gameModel);
+
+    switch (validationResult.type) {
       case ActionType.START_DRAW_TIME: {
-        validateStartDrawTimeAction(gameModel, userId);
         return handleStartDrawTimeAction(userId, gameModel);
       }
       case ActionType.START_ENERGY_TIME: {
-        validateStartEnergyTimeAction(gameModel, userId);
         return handleStartEnergyTimeAction(userId, gameModel);
       }
       case ActionType.START_PUT_TIME: {
-        validateStartPutTimeAction(gameModel, userId);
         return handleStartPutTimeAction(gameModel);
       }
       case ActionType.PUT_SOUL: {
-        const payload: PutSoulActionPayload = validatePutSoulAction(data, gameModel, userId);
-        return handlePutSoulAction(userId, payload, gameModel);
+        return handlePutSoulAction(userId, validationResult.payload, gameModel);
       }
       case ActionType.START_SOMETHING_TIME: {
-        validateStartSomethingTimeAction(gameModel, userId);
         return handleStartSomethingTimeAction(gameModel);
       }
       case ActionType.SUMMON_MONSTER: {
         return handleSummonMonsterAction(userId, data, gameModel);
       }
       case ActionType.START_BATTLE_TIME: {
-        validateStartBattleTimeAction(gameModel, userId);
         return handleStartBattleTimeAction(gameModel);
       }
       case ActionType.START_END_TIME: {
-        validateStartEndTimeAction(gameModel, userId);
         return handleStartEndTimeAction(gameModel);
       }
       case ActionType.ATTACK: {
-        const payload = validateAttackAction(data, gameModel, userId);
-        return handleAttackAction(userId, payload, gameModel);
+        return handleAttackAction(userId, validationResult.payload, gameModel);
       }
       case ActionType.FINISH_END_TIME: {
-        const payload = validateFinishEndTimeAction(gameModel, userId);
-        return handleFinishEndTimeAction(payload, gameModel);
+        return handleFinishEndTimeAction(validationResult.payload, gameModel);
       }
       case ActionType.EFFECT_RUTERUTE_DRAW: {
-        const payload = validateEffectRuteruteDrawAction(data, gameModel);
-        return handleEffectRuteruteDraw(userId, payload, gameModel);
+        return handleEffectRuteruteDraw(userId, validationResult.payload, gameModel);
       }
       default: {
         return gameModel;
---
+++ server/src/game/actions/validators/index.ts
@@ -0,0 +1,85 @@
+import { GameModel } from '../../../models/game.model';
+import { GameActionDispatchInput } from '../../../graphql/index';
+import { ActionType } from '../../../graphql/index';
+import { validateStartDrawTimeAction } from '../validators/startDrawTime';
+import { validateStartEnergyTimeAction } from '../validators/startEnergyTime';
+import { validateStartPutTimeAction } from '../validators/startPutTime';
+import { validatePutSoulAction } from '../validators/putSoul';
+import { validateAttackAction } from '../validators/attack';
+import { validateStartEndTimeAction } from '../validators/startEndTime';
+import { validateStartSomethingTimeAction } from '../validators/startSomethingTime';
+import { validateStartBattleTimeAction } from '../validators/startBattleTime';
+import { validateFinishEndTimeAction } from '../validators/finishEndTime';
+import { validateEffectRuteruteDrawAction } from '../validators/effectRuteruteDraw';
+import { PutSoulActionPayload } from '../handlers/putSoul';
+import { AttackActionPayload } from '../handlers/attack';
+import { FinishEndTimeActionPayload } from '../handlers/finishEndTime';
+import { EffectRuteruteDrawActionPayload } from '../handlers/effectRuteruteDraw';
+
+type ValidationResult =
+  | { type: ActionType.START_DRAW_TIME }
+  | { type: ActionType.START_ENERGY_TIME }
+  | { type: ActionType.START_PUT_TIME }
+  | { type: ActionType.PUT_SOUL; payload: PutSoulActionPayload }
+  | { type: ActionType.START_SOMETHING_TIME }
+  | { type: ActionType.SUMMON_MONSTER; payload: GameActionDispatchInput }
+  | { type: ActionType.START_BATTLE_TIME }
+  | { type: ActionType.START_END_TIME }
+  | { type: ActionType.ATTACK; payload: AttackActionPayload }
+  | { type: ActionType.FINISH_END_TIME; payload: FinishEndTimeActionPayload }
+  | { type: ActionType.EFFECT_RUTERUTE_DRAW; payload: EffectRuteruteDrawActionPayload };
+
+export const validateAction = (
+  data: GameActionDispatchInput,
+  userId: string,
+  gameModel: GameModel,
+): ValidationResult => {
+  switch (data.type) {
+    case ActionType.START_DRAW_TIME: {
+      validateStartDrawTimeAction(gameModel, userId);
+      return { type: ActionType.START_DRAW_TIME };
+    }
+    case ActionType.START_ENERGY_TIME: {
+      validateStartEnergyTimeAction(gameModel, userId);
+      return { type: ActionType.START_ENERGY_TIME };
+    }
+    case ActionType.START_PUT_TIME: {
+      validateStartPutTimeAction(gameModel, userId);
+      return { type: ActionType.START_PUT_TIME };
+    }
+    case ActionType.PUT_SOUL: {
+      const payload: PutSoulActionPayload = validatePutSoulAction(data, gameModel, userId);
+      return { type: ActionType.PUT_SOUL, payload };
+    }
+    case ActionType.START_SOMETHING_TIME: {
+      validateStartSomethingTimeAction(gameModel, userId);
+      return { type: ActionType.START_SOMETHING_TIME };
+    }
+    case ActionType.SUMMON_MONSTER: {
+      return { type: ActionType.SUMMON_MONSTER, payload: data };
+    }
+    case ActionType.START_BATTLE_TIME: {
+      validateStartBattleTimeAction(gameModel, userId);
+      return { type: ActionType.START_BATTLE_TIME };
+    }
+    case ActionType.START_END_TIME: {
+      validateStartEndTimeAction(gameModel, userId);
+      return { type: ActionType.START_END_TIME };
+    }
+    case ActionType.ATTACK: {
+      const payload = validateAttackAction(data, gameModel, userId);
+      return { type: ActionType.ATTACK, payload };
+    }
+    case ActionType.FINISH_END_TIME: {
+      const payload = validateFinishEndTimeAction(gameModel, userId);
+      return { type: ActionType.FINISH_END_TIME, payload };
+    }
+    case ActionType.EFFECT_RUTERUTE_DRAW: {
+      const payload = validateEffectRuteruteDrawAction(data, gameModel);
+      return { type: ActionType.EFFECT_RUTERUTE_DRAW, payload };
+    }
+    default: {
+      throw new Error('Invalid action type');
+    }
+  }
+};
