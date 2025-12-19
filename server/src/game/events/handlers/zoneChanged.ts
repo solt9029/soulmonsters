7abff4e24c7ad7faae52ab8bfe6f3b4b2a41cae6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-12-20 01:24:38 +0900

    Implement バクボムダン

--- server/src/game/events/handlers/zoneChanged.ts
+++ server/src/game/events/handlers/zoneChanged.ts
@@ -3,6 +3,9 @@ import { GameModel } from '../../../models/game.model';
 import { Zone } from '../../../graphql';
 import { addUserEnergy } from './zoneChanged/addUserEnergy';
 import { subtractUserEnergy } from './zoneChanged/subtractUserEnergy';
+// TODO: dealDamageToPlayerの定義場所を共通utilsまたはzoneChangedディレクトリに移動する
+// actionのhandlerのための関数がeventのhandlerの中で使われているのは構造的に違和感がある
+import { dealDamageToPlayer } from '../../actions/handlers/attack/dealDamageToPlayer';
 
 export function handleZoneChanged(event: ZoneChangedEvent, gameModel: GameModel): GameModel {
   const movedCard = gameModel.gameCards.find(gc => gc.id === event.gameCardId);
@@ -20,6 +23,14 @@ export function handleZoneChanged(event: ZoneChangedEvent, gameModel: GameModel)
 
   if (event.fromZone === Zone.BATTLE && event.toZone === Zone.SOUL && movedCard.card.id === 14) {
     gameModel = addUserEnergy(gameModel, movedCard.currentUserId, 2);
+  }
+
+  // バクボムダン
+  if (event.fromZone === Zone.BATTLE && event.toZone === Zone.SOUL && movedCard.card.id === 7) {
+    const opponentUserId = gameModel.gameUsers.find(gu => gu.userId !== movedCard.currentUserId)?.userId;
+    if (opponentUserId) {
+      gameModel = dealDamageToPlayer(gameModel, opponentUserId, 600);
+    }
   }
 
   return gameModel;
