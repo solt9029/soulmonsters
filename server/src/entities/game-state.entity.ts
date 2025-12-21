67499a42c093d03800c5cf5f3c7bfadcce319e45
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-12-21 09:24:37 +0900

    Update client & fix server

--- server/src/entities/game-state.entity.ts
+++ server/src/entities/game-state.entity.ts
@@ -20,6 +20,10 @@ type State =
   | {
       type: StateType.EFFECT_RUTERUTE_DRAW_COUNT;
       data: { value: number };
+    }
+  | {
+      type: StateType.EFFECT_FRESH_FISH_DRAW_COUNT;
+      data: { value: number };
     };
 
 @Entity({ name: 'gameStates' })
