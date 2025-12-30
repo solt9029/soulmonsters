db570512592efea9d25915ddd44f7882447a5ecb
Kenshi Shiode <atemu1212@yahoo.co.jp>
2025-12-30 16:46:24 +0900

    wip

--- client/src/constants/action-step-alert-messages.ts
+++ client/src/constants/action-step-alert-messages.ts
@@ -2,4 +2,8 @@ export const ActionStepAlertMessages = {
   COMPLETED: '',
   SELECT_ATTACK_TARGET: '攻撃対象のカードまたはユーザを選択してください',
   SELECT_POWER_DOWN_TARGET: 'パワーダウンさせる対象のカードを選択してください',
+  SELECT_SOUL_CANON_COST:
+    'コストとなるソウルゾーンのカードを4枚選択してください',
+  SELECT_SOUL_CANON_TARGET:
+    '効果対象となる相手バトルゾーンのカードを選択してください',
 };
