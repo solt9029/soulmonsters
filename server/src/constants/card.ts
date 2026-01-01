7f4feeaae111562b8af3f34ab22de667458ffeae
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-01-01 21:52:41 +0900

    Delete todo

--- server/src/constants/card.ts
+++ server/src/constants/card.ts
@@ -8,7 +8,7 @@ export const CARD_ID = {
   SHINKASHITABAKUBOMDAN: 7, // 進化したバクボムダン
   NATSUKASHINORUDE: 8, // 懐かしのルード
   HAMONTAKINIKARARENUMONO: 9, // ハモンタキーに狩られぬもの todo
-  SUPERNEWVOLTS: 10, // スーパーニューボルツ todo
+  SUPERNEWVOLTS: 10, // スーパーニューボルツ
   REITETSUNATOTI: 11, // 冷徹な鳥
   AIKAWARAZUYOKUWAKARANAIHANA: 12, // 相変わらずよく分からない花
   SHIMASHIMAJUNIOR: 13, // シマシマジュニア
