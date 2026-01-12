fc92a8dbdb4176056a752b773fb15e1cf5d67112
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-01-13 00:09:43 +0900

    Delete todo

--- server/src/models/game-state.model.ts
+++ server/src/models/game-state.model.ts
@@ -42,6 +42,6 @@ export class GameStateModel {
   id: number;
   gameCardId: number;
   state: State;
-  createdAt: Date; // TODO: いらないかも
-  updatedAt: Date; // TODO: いらないかも
+  createdAt: Date;
+  updatedAt: Date;
 }
