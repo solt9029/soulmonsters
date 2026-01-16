680533e01e33c7e964cfdf238f6af9927ad7d9bc
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-01-16 12:41:09 +0900

    Use saveEffectUseCountGameState

--- server/src/game/chains/resolvers/ruteruteDraw.ts
+++ server/src/game/chains/resolvers/ruteruteDraw.ts
@@ -1,8 +1,7 @@
 import { GameModel } from 'src/models/game.model';
 import { GameChainLinkModel } from 'src/models/game-chain-link.model';
-import { GameStateModel } from 'src/models/game-state.model';
-import { StateType } from 'src/graphql/index';
 import { drawCardFromDeck } from 'src/game/actions/handlers/effectRuteruteDraw/drawCardFromDeck';
+import { saveEffectUseCountGameState } from 'src/game/actions/handlers/effectRuteruteDraw/saveEffectUseCountGameState';
 
 export const resolveRuteruteDraw = (gameModel: GameModel, gameChainLink: GameChainLinkModel): GameModel => {
   const gameCard = gameModel.gameCards.find(gc => gc.id === gameChainLink.gameCardId);
@@ -11,32 +10,7 @@ export const resolveRuteruteDraw = (gameModel: GameModel, gameChainLink: GameCha
   }
 
   gameModel = drawCardFromDeck(gameModel, gameChainLink.userId);
-
-  const existsState = gameModel.gameStates.find(
-    gs => gs.state.type === StateType.EFFECT_RUTERUTE_DRAW_COUNT && gs.gameCardId === gameCard.id,
-  );
-
-  if (existsState) {
-    gameModel.gameStates = gameModel.gameStates.map(gs =>
-      gs.state.type === StateType.EFFECT_RUTERUTE_DRAW_COUNT && gs.gameCardId === gameCard.id
-        ? new GameStateModel({
-            ...gs,
-            state: {
-              type: StateType.EFFECT_RUTERUTE_DRAW_COUNT,
-              data: { value: gs.state.data.value + 1 },
-            },
-          })
-        : gs,
-    );
-  } else {
-    const newGameState = new GameStateModel({
-      gameCardId: gameCard.id,
-      state: { type: StateType.EFFECT_RUTERUTE_DRAW_COUNT, data: { value: 1 } },
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    gameModel.gameStates = [...gameModel.gameStates, newGameState];
-  }
+  gameModel = saveEffectUseCountGameState(gameModel, gameCard);
 
   return gameModel;
 };
