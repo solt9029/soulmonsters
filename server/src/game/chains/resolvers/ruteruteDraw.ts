a5572c2bece780dd1cd8252befaad646049ebc3b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-01-16 12:46:22 +0900

    Move file

--- server/src/game/chains/resolvers/ruteruteDraw.ts
+++ server/src/game/chains/resolvers/ruteruteDraw.ts
@@ -1,6 +1,6 @@
 import { GameModel } from 'src/models/game.model';
 import { GameChainLinkModel } from 'src/models/game-chain-link.model';
-import { drawCardFromDeck } from 'src/game/actions/handlers/effectRuteruteDraw/drawCardFromDeck';
+import { drawCardFromDeck } from 'src/game/utils/drawCardFromDeck';
 import { saveEffectUseCountGameState } from 'src/game/actions/handlers/effectRuteruteDraw/saveEffectUseCountGameState';
 
 export const resolveRuteruteDraw = (gameModel: GameModel, gameChainLink: GameChainLinkModel): GameModel => {
