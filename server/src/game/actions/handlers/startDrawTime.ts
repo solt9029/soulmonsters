b8b52d2f6b47d9d3a9dbcce86923856de057ad88
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-01-24 10:34:57 +0900

    Delete updateGamePhaseAndTurn

--- server/src/game/actions/handlers/startDrawTime.ts
+++ server/src/game/actions/handlers/startDrawTime.ts
@@ -1,9 +1,10 @@
 import { GameModel } from '../../../models/game.model';
 import { drawCardFromDeck } from 'src/game/mutations/drawCardFromDeck';
-import { updateGamePhaseAndTurn } from '../../mutations/startDrawTime/updateGamePhaseAndTurn';
+import { Phase } from 'src/graphql';
 
 export function handleStartDrawTimeAction(userId: string, gameModel: GameModel): GameModel {
-  gameModel = updateGamePhaseAndTurn(gameModel);
+  gameModel.phase = Phase.DRAW;
+  gameModel.turnCount = gameModel.turnCount + 1;
   gameModel = drawCardFromDeck(gameModel, userId);
   return gameModel;
 }
