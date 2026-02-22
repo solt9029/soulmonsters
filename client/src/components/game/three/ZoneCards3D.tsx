8a4936ba055d7b26b54d9cdb5a880fb240fecf74
Kenshi Shiode <atemu1212@yahoo.co.jp>
2026-02-22 17:08:21 +0900

    Switch morgue & deck

--- client/src/components/game/three/ZoneCards3D.tsx
+++ client/src/components/game/three/ZoneCards3D.tsx
@@ -13,13 +13,13 @@ const ZONE_POSITIONS: Record<ZoneKey, [number, number, number]> = {
   [`${Zone.Hand}_player`]: [0, 0, 7.75],
   [`${Zone.Battle}_player`]: [0, 0, 2],
   [`${Zone.Soul}_player`]: [-0, 0, 5],
-  [`${Zone.Morgue}_player`]: [5.5, 0, 5],
-  [`${Zone.Deck}_player`]: [5.5, 0, 2],
+  [`${Zone.Morgue}_player`]: [5.5, 0, 2],
+  [`${Zone.Deck}_player`]: [5.5, 0, 5],
   [`${Zone.Hand}_opponent`]: [0, 0, -7.75],
   [`${Zone.Battle}_opponent`]: [0, 0, -2],
   [`${Zone.Soul}_opponent`]: [0, 0, -5],
-  [`${Zone.Morgue}_opponent`]: [-5.5, 0, -5],
-  [`${Zone.Deck}_opponent`]: [-5.5, 0, -2],
+  [`${Zone.Morgue}_opponent`]: [-5.5, 0, -2],
+  [`${Zone.Deck}_opponent`]: [-5.5, 0, -5],
 };
 
 const STACK_ZONES = new Set([Zone.Deck, Zone.Morgue]);
